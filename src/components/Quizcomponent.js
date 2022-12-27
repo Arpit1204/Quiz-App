25a311e2275a9f3bd376e41934c61a568332476b
Arpit <gulati.arpit0055@gmail.com>
2022-12-27 12:24:58 +0530

    first commit

--- src/components/Quizcomponent.js
+++ src/components/Quizcomponent.js
@@ -1,24 +1,53 @@
 import './quiz.css';
 import React, { Component } from 'react'
+import  customData from "./questions.json";
 
 export default class Quizcomponent extends Component {
+  constructor(){
+    super();
+    this.state={
+        currentQuestion:0,
+    }
+}
+
+changeQuestion(isForward){
+    this.setState((prevState)=>{
+        let comingQuestion=prevState.currentQuestion;
+        comingQuestion=isForward?comingQuestion+1:comingQuestion-1;
+        if(comingQuestion<0){
+            comingQuestion=0;
+        }
+        else if(comingQuestion>14){
+            comingQuestion=14;
+        }
+        return {
+            currentQuestion: comingQuestion,
+        };
+    })
+}
+
+quit(){
+  alert("Are you sure you want to quit?")
+  window.location.reload();
+}
   render() {
+    let data = customData[this.state.currentQuestion];
     return (
       <div className='container'>
         <div id='quistion-container'>
             <h2 id='ques-head'>Question</h2>
-            <p id='question-number'>1 of 15</p>
-            <p id='question'>What is the only mammel that can jump?</p>
+            <p id='question-number'>{this.state.currentQuestion+1} of 15</p>
+            <p id='question'>{data.question}</p>
             <div className='option-div'>
-                <div className='options'>Dog</div>
-                <div className='options'>Elephant</div>
-                <div className='options'>Goat</div>
-                <div className='options'>Lion</div>
+                <div className='options'>{data.optionA}</div>
+                <div className='options'>{data.optionB}</div>
+                <div className='options'>{data.optionC}</div>
+                <div className='options'>{data.optionD}</div>
             </div>
             <div className='button-div'>
-                <button className='butt' id='button1'>Previous</button>
-                <button className='butt' id='button2'>Next</button>
-                <button className='butt' id='button3'>Quit</button>
+                <button className='butt' id='button1' onClick={()=>this.changeQuestion(false)}>Previous</button>
+                <button className='butt' id='button2' onClick={()=>this.changeQuestion(true)}>Next</button>
+                <button className='butt' id='button3' onClick={()=>this.quit()}>Quit</button>
             </div>
         </div>
 
